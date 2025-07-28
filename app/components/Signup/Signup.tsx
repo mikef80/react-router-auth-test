805bc9db64b597e2e9158911635baeda3dfb729a
Mike <mike@mike-francis.org>
2025-07-28 22:30:55 +0100

    corrected error with id on signout form

--- app/components/Signup/Signup.tsx
+++ app/components/Signup/Signup.tsx
@@ -8,7 +8,7 @@ const Signup = () => {
     <fetcher.Form action='/signup' method='post'>
       <label htmlFor='signupemail'>Email:</label>
       <input type='email' name='signupemail' id='signupemail' />
-      <label htmlFor='password'>Password:</label>
+      <label htmlFor='signuppassword'>Password:</label>
       <input type='password' name='signuppassword' id='signuppassword' />
       <label htmlFor='signupconfirmpassword'>Confirm Password:</label>
       <input type='password' name='signupconfirmpassword' id='signupconfirmpassword' />
