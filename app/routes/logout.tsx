db4341aafbeea5982a729df351fcb1f01fabcbdf
Mike <mike@mike-francis.org>
2025-07-28 22:39:49 +0100

    tidying logout slightly

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,14 +1,16 @@
 import { createClient } from "~/utils/supabase.server";
 import type { Route } from "../+types/root";
+import { redirect } from "react-router";
 
 export const action = async ({ request }: Route.ClientActionArgs) => {
   const { supabase, headers } = createClient(request);
-  const { error } = await supabase.auth.signOut({ scope: "local" });
+  const { error } = await supabase.auth.signOut();
 
   if (error) {
     return new Response("Sign out failed", { status: 401 });
   }
 
-  headers.set("Location", "/");
-  return new Response(null, { status: 302, headers });
+  // headers.set("Location", "/");
+  // return new Response(null, { status: 302, headers });
+  return redirect("/", { headers });
 };
