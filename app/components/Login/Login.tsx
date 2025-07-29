16a0ba1095499393c89679b3c021e7d4f78c6e09
Mike <mike@mike-francis.org>
2025-07-29 22:22:23 +0100

    updated login so that it uses state and updates dynamically with page reload, but i can't help feeling this is the wrong approach

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -1,16 +1,51 @@
-import { useFetcher } from "react-router";
+import { useFetcher, useNavigate } from "react-router";
 import "./Login.css";
+import { useState } from "react";
+import { supabase } from "~/utils/supabase.client";
 
 const Login = () => {
   const fetcher = useFetcher();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrorMsg("");
+
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error) {
+      setErrorMsg(error.message);
+      return;
+    }
+
+    // navigate("/protectedroute");
+  };
 
   return (
-    <fetcher.Form action='/login' method='post'>
+    <fetcher.Form onSubmit={handleSubmit}>
       <label htmlFor='loginemail'>Email:</label>
-      <input type='email' name='loginemail' id='loginemail' />
+      <input
+        type='email'
+        name='loginemail'
+        id='loginemail'
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
       <label htmlFor='loginpassword'>Password:</label>
-      <input type='password' name='loginpassword' id='loginpassword' />
-      <button type='submit'>Log In</button>
+      <input
+        type='password'
+        name='loginpassword'
+        id='loginpassword'
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type='submit' disabled={!email || !password}>
+        Log In
+      </button>
+      {errorMsg && <p style={{ color: "red" }}>{errorMsg}</p>}
     </fetcher.Form>
   );
 };
